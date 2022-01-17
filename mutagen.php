875bae378b5d613ba1e0eec78b6eee51ba05d8a2
vzeufd <support@mutagen.ru>
2022-01-17 17:17:35 +0300

    Update mutagen.php

--- mutagen.php
+++ mutagen.php
@@ -2,7 +2,7 @@
 
 include('IXR_Library.inc.php');
 
-$ljClient = new IXR_Client('mutagen.ru', '/xmlrpc');
+$ljClient = new IXR_Client('api.mutagen.ru', '/xmlrpc');
 
 $mutagen = new mutagen($ljClient);
 
